4f58115902b9632492fb45811ee34f69be8bfff4
EasyS2021 <78708906+EasyS2021@users.noreply.github.com>
2021-02-08 07:55:24 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,57 +1,100 @@
-﻿using ImageMagick;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageMagick;
 using System.IO;
+using System.Threading;
 
 namespace HEICtoJPG
 {
+
 	class Program
 	{
+		public static string imageType;
+
 		static void Main(string[] args)
 		{
-			int fileLeft;
+			int fileLeft = 0;
+			string sourcePath = "", targetPath = "";
+			string[] filesInFolder;
+			bool flag = false;
 			foreach (var arg in args)
 			{
 				Console.WriteLine("Checking directory: " + arg);
-				if (Directory.Exists(arg))
+				
+				if(arg.Contains("/source") )sourcePath = GetPath(arg);
+				if (arg.Contains("/target")) targetPath = GetPath(arg);
+				
+				
+				if (arg.Contains("/delete")) flag = true;
+				if (arg.Contains("/jpg")) imageType = "jpg";
+				if (arg.Contains("/png")) imageType = "png";
+			}
+			if(sourcePath=="") sourcePath= Directory.GetCurrentDirectory();
+			if (targetPath == "") targetPath = Directory.GetCurrentDirectory();
+			
+			filesInFolder = Directory.GetFiles(sourcePath);
+			foreach (var file in filesInFolder)
+			{
+				string ext = Path.GetExtension(file).ToLower();
+				if (ext == ".heic") fileLeft++;
+			}
+			foreach (var file in filesInFolder)
+			{
+				string ext = Path.GetExtension(file).ToLower();
+				if (ext == ".heic")
 				{
-					string exportPath = Path.Combine(arg, "jpg");
-					if (!Directory.Exists(exportPath))
-						Directory.CreateDirectory(exportPath);
-					var filesInFolder = Directory.GetFiles(arg);
-					fileLeft = filesInFolder.Length;
-					foreach (var file in filesInFolder)
-					{
-						ConvertImage(file, exportPath);
-						Console.WriteLine(--fileLeft + " files left");
-					}
+					Console.WriteLine(file);
+					ConvertImage(file, targetPath);
 				}
 			}
-			Console.WriteLine("Press AnyKey to continue");
-			Console.ReadKey();
-		}
+	
+			foreach (var file in filesInFolder)
+			{
+				string ext = Path.GetExtension(file).ToLower();
+				if (ext == ".heic" && flag && File.Exists(file))
+				{
+					File.Delete(file);
+					System.IO.File.Delete(file);
 
+				}
+			}
+		
+		}
 		static void ConvertImage(string fileToConvert, string exportPath)
 		{
-			string exportFilePath = Path.Combine(exportPath, Path.GetFileNameWithoutExtension(Path.GetFileName(fileToConvert))) + ".jpg";
+			string exportFilePath = Path.Combine(exportPath, Path.GetFileNameWithoutExtension(Path.GetFileName(fileToConvert))) + "."+imageType;
+			string outFilename = Path.GetFileName(exportFilePath);
+			string ext = Path.GetExtension(outFilename);
+			string imageExtension = Path.GetExtension(fileToConvert).ToLower();
+			string inFilename = Path.GetFileName(fileToConvert);
+
 			if (File.Exists(exportFilePath))
 			{
-				Console.WriteLine("File already exist " + exportFilePath);
+				Console.WriteLine("Skipped file  " + inFilename + " becuase it has already been converted.");
 				return;
 			}
-			string imageExtension = Path.GetExtension(fileToConvert).ToLower();
 			if (imageExtension.Contains("heic") || imageExtension.Contains("png"))
 			{
-				if (File.Exists(exportFilePath))
-					Console.WriteLine("File already exist " + exportFilePath);
-				Console.Write("Converting " + fileToConvert + "...");
-				using (MagickImage image = new MagickImage(fileToConvert))
+				Console.Write("Processing Item " + inFilename + "...");
+				
+				using (MagickImage image = new MagickImage(File.ReadAllBytes(fileToConvert)))
 				{
 					image.Write(exportFilePath);
 					Console.WriteLine("Ok");
+					
 				}
 			}
-			else if (imageExtension.Contains("jpg"))
-				File.Copy(fileToConvert, exportFilePath);
+			
+		}
+		static string GetPath(string str)
+		{
+			
+			string[] ptr = str.Split(new char[] { '=' });
+			return ptr[1];
+
 		}
 	}
 }
